9bcbb0f2b1492b2893a6ab6737912c4d23d67af3
Abbilaash A T <78356376+Abbilaash@users.noreply.github.com>
2024-02-23 00:05:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -64,5 +64,5 @@ def fetch_messages():
 
 
 
-if __name__ == '__main__':
-    socketio.run(app, debug=True,host='0.0.0.0')
+'''if __name__ == '__main__':
+    socketio.run(app, debug=True,host='0.0.0.0')'''
